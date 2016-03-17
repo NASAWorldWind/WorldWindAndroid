aed9d292966b2f839e243421c440c5a458613b13
Bruce Schubert <bruce@emxsys.com>
2016-03-17 10:30:51 -0700

    Added unit test stubs to SectorTest.

--- worldwind/src/test/java/gov/nasa/worldwind/geom/SectorTest.java
+++ worldwind/src/test/java/gov/nasa/worldwind/geom/SectorTest.java
@@ -5,11 +5,13 @@
 
 package gov.nasa.worldwind.geom;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SectorTest {
+
     @Test
     public void testConstructor_default() throws Exception {
         Sector sector = new Sector();
@@ -18,6 +20,198 @@ public class SectorTest {
         assertEquals("maxLongitude", 0d, sector.maxLongitude, 0d);
         assertEquals("minLatitude", 0d, sector.minLatitude, 0d);
         assertEquals("minLongitude", 0d, sector.minLongitude, 0d);
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testConstructor_Copy() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testFromDegrees() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testFromRadians() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testEquals() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testHashCode() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testIsEmpty() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testDeltaLatitude() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testDeltaLongitude() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testCentroidLatitude() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testCentroidLongitude() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testCentroid() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testSet() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testSet1() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testSetEmpty() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testSetFullSphere() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testIntersects() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testIntersects1() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testIntersect() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testIntersect1() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testContains() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testContains1() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testContains2() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testUnion() throws Exception {
+
+        fail("The test case is a stub.");
+
+    }
+
+    @Ignore("not implemented")
+    @Test
+    public void testUnion1() throws Exception {
+
+        fail("The test case is a stub.");
+
     }
 }
 
