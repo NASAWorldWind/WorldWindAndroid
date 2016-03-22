a81078635b2eb38671d94dc47b756b2bfbb4690a
pdavidc <paul@paulcollinssoftware.com>
2016-03-22 16:45:16 -0700

    Added constants for the gesture package.
    The gesture package provides a collection of classes that detect common multi-touch gestures in a manner appropriate for use with World Wind touch navigation.

--- worldwind/src/main/java/gov/nasa/worldwind/WorldWind.java
+++ worldwind/src/main/java/gov/nasa/worldwind/WorldWind.java
@@ -19,6 +19,24 @@ public class WorldWind {
     public static final int ABSOLUTE = 0;
 
     /**
+     * The BEGAN gesture recognizer state. Continuous gesture recognizers transition to this state from the POSSIBLE
+     * state when the gesture is first recognized.
+     */
+    public static final int BEGAN = 3;
+
+    /**
+     * The CANCELLED gesture recognizer state. Continuous gesture recognizers may transition to this state from the
+     * BEGAN state or the CHANGED state when the touch events are cancelled.
+     */
+    public static final int CANCELLED = 5;
+
+    /**
+     * The CHANGED gesture recognizer state. Continuous gesture recognizers transition to this state from the BEGAN
+     * state or the CHANGED state, whenever an input event indicates a change in the gesture.
+     */
+    public static final int CHANGED = 4;
+
+    /**
      * Altitude mode constant indicating an altitude on the terrain. Ignores a position's specified altitude, and always
      * places the position on the terrain.
      */
@@ -31,6 +49,18 @@ public class WorldWind {
     public static final int RELATIVE_TO_GROUND = 2;
 
     /**
+     * The ENDED gesture recognizer state. Continuous gesture recognizers transition to this state from either the BEGAN
+     * state or the CHANGED state when the current input no longer represents the gesture.
+     */
+    public static final int ENDED = 6;
+
+    /**
+     * The FAILED gesture recognizer state. Gesture recognizers transition to this state from the POSSIBLE state when
+     * the gesture cannot be recognized given the current input.
+     */
+    public static final int FAILED = 1;
+
+    /**
      * Path type constant indicating a great circle arc between two locations.
      */
     public static final int GREAT_CIRCLE = 0;
@@ -39,6 +69,18 @@ public class WorldWind {
      * Path type constant indicating simple linear interpolation between two locations.
      */
     public static final int LINEAR = 1;
+
+    /**
+     * The POSSIBLE gesture recognizer state. Gesture recognizers in this state are idle when there is no input event to
+     * evaluate, or are evaluating input events to determine whether or not to transition into another state.
+     */
+    public static final int POSSIBLE = 0;
+
+    /**
+     * The RECOGNIZED gesture recognizer state. Discrete gesture recognizers transition to this state from the POSSIBLE
+     * state when the gesture is recognized.
+     */
+    public static final int RECOGNIZED = 2;
 
     /**
      * Path type constant indicating a line of constant bearing between two locations.
@@ -62,6 +104,16 @@ public class WorldWind {
     @IntDef({GREAT_CIRCLE, LINEAR, RHUMB_LINE})
     @Retention(RetentionPolicy.SOURCE)
     public @interface PathType {
+
+    }
+
+    /**
+     * Gesture state indicates a GestureRecognizer's current state. Accepted values are {@link #POSSIBLE}, {@link
+     * #FAILED}, {@link #RECOGNIZED}, {@link #BEGAN}, {@link #CHANGED}, {@link #CANCELLED}, and {@link #ENDED}.
+     */
+    @IntDef({POSSIBLE, FAILED, RECOGNIZED, BEGAN, CHANGED, CANCELLED, ENDED})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface GestureState {
 
     }
 }
