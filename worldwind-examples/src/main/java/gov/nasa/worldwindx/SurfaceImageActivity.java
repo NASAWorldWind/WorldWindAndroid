fe96a7ea7ebbc898200415c6b76eb35521d3fe0c
pdavidc <paul@paulcollinssoftware.com>
2016-03-28 09:08:11 -0700

    Added an example acivity for SurfaceImage

---
+++ worldwind-examples/src/main/java/gov/nasa/worldwindx/SurfaceImageActivity.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration. All Rights Reserved.
+ */
+
+package gov.nasa.worldwindx;
+
+import android.os.Bundle;
+
+import gov.nasa.worldwind.geom.Sector;
+import gov.nasa.worldwind.layer.RenderableLayer;
+import gov.nasa.worldwind.shape.SurfaceImage;
+
+public class SurfaceImageActivity extends BasicActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Configure a Surface Image to display an Android resource showing the NASA logo.
+        Sector sector = new Sector(37.46, 15.5, 0.5, 0.6);
+        int resourceId = R.drawable.nasa_logo;
+        SurfaceImage surfaceImageResource = new SurfaceImage(sector, resourceId);
+
+        // Configure a Surface Image to display a remote image showing Mount Etna erupting on July 13th, 2001.
+        // TODO sector constructor from North, South, East, West coordinates
+        sector = new Sector(37.46543388598137, 14.60128369746704, 0.45360804083528, 0.75704283995502);
+        String urlString = "http://kml-samples.googlecode.com/svn/trunk/resources/etna.jpg";
+        SurfaceImage surfaceImageUrl = new SurfaceImage(sector, urlString);
+
+        // Add a World Window layer that displays the Surface Image, just before the Atmosphere layer.
+        RenderableLayer layer = new RenderableLayer("Surface Image");
+        layer.addRenderable(surfaceImageResource);
+        layer.addRenderable(surfaceImageUrl);
+        int index = this.getWorldWindow().getLayers().indexOfLayerNamed("Atmosphere");
+        this.getWorldWindow().getLayers().addLayer(index, layer);
+
+        // Position the viewer so that the Surface Images are visible when the activity is created.
+        this.getWorldWindow().getNavigator().setLatitude(37.46543388598137);
+        this.getWorldWindow().getNavigator().setLongitude(14.97980511744455);
+        this.getWorldWindow().getNavigator().setAltitude(4.0e5);
+    }
+}
