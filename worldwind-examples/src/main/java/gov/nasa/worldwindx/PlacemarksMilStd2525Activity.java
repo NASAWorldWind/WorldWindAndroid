b0680dd90cfe9149f51aba1e8a989aa013e23748
Bruce Schubert <bruce@emxsys.com>
2016-04-28 15:12:13 -0700

    Using WorldWind.taskService in PlacemarksMilStd2525Activity.

--- worldwind-examples/src/main/java/gov/nasa/worldwindx/PlacemarksMilStd2525Activity.java
+++ worldwind-examples/src/main/java/gov/nasa/worldwindx/PlacemarksMilStd2525Activity.java
@@ -25,17 +25,13 @@ import gov.nasa.worldwindx.milstd2525.MilStd2525Placemark;
 
 public class PlacemarksMilStd2525Activity extends BasicGlobeActivity {
 
-    protected static Executor executorService = Executors.newSingleThreadExecutor();
-
-    protected Handler activityHandler = new Handler();
-
     protected RenderableLayer symbolLayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setAboutBoxTitle("About the " + getResources().getText(R.string.title_placemarks));
-        setAboutBoxText("Demonstrates how to add Placemarks to a RenderableLayer.");
+        setAboutBoxText("Demonstrates how to add MilStd2525C Symbols to a RenderableLayer.");
 
         // Create a layer for the military symbols and add it just before the Atmosphere layer
         this.symbolLayer = new RenderableLayer("Symbols");
@@ -43,29 +39,34 @@ public class PlacemarksMilStd2525Activity extends BasicGlobeActivity {
         int index = layers.indexOfLayerNamed("Atmosphere");
         layers.addLayer(index, this.symbolLayer);
 
+        // The rendering service takes time initialize. We'll display the globe
+        // while the service is initializing on a background thread.
+        initializeSymbols();
+
         // Set the camera to look at the area where the symbols will be displayed.
         Position pos = new Position(32.4520, 63.44553, 0);
         LookAt lookAt = new LookAt().set(pos.latitude, pos.longitude, pos.altitude, WorldWind.ABSOLUTE,
             1e5 /*range*/, 0 /*heading*/, 45 /*tilt*/, 0 /*roll*/);
         this.getWorldWindow().getNavigator().setAsLookAt(this.getWorldWindow().getGlobe(), lookAt);
 
-        // The rendering service takes time initialize. We'll display the globe while the service is initializing
-        // on a background thread.
-        initializeSymbols();
     }
 
+    /**
+     * Initializes the symbol renderer on a background thread and loads the symbols after the initialization is
+     * complete.
+     */
     private void initializeSymbols() {
-        executorService.execute(new Runnable() {
+        WorldWind.taskService().execute(new Runnable() {
             @Override
             public void run() {
                 // The symbol renderer can take a long time to initialize.
                 MilStd2525Renderer.initialize(getApplicationContext());
 
-                //
-                activityHandler.post(new Runnable() {
+                // We must add the new renderables on the GLThread.
+                // TODO: demonstrate an efficient/foolproof way of adding renderables to a running application.
+                getWorldWindow().queueEvent(new Runnable() {
                     @Override
                     public void run() {
-
                         // "MIL-STD-2525 Friendly SOF Drone Aircraft"
                         SparseArray<String> modifiers = new SparseArray<String>();
                         modifiers.put(ModifiersUnits.Q_DIRECTION_OF_MOVEMENT, "235");
@@ -73,8 +74,7 @@ public class PlacemarksMilStd2525Activity extends BasicGlobeActivity {
                             Position.fromDegrees(32.4520, 63.44553, 3000),
                             "SFAPMFQM--GIUSA", modifiers);
                         symbolLayer.addRenderable(drone);
-                        symbolLayer.addRenderable(Placemark.simple(drone.getPosition(), new Color(1, 1, 1, 1), 4)); // for placement verification
-
+                        //symbolLayer.addRenderable(Placemark.simple(drone.getPosition(), new Color(1, 1, 1, 1), 4)); // for placement verification
 
                         // "MIL-STD-2525 Hostile Self-Propelled Rocket Launchers"
                         modifiers.clear();
@@ -84,7 +84,7 @@ public class PlacemarksMilStd2525Activity extends BasicGlobeActivity {
                             Position.fromDegrees(32.4014, 63.3894, 0),
                             "SHGXUCFRMS----G", modifiers);
                         symbolLayer.addRenderable(launcher);
-                        symbolLayer.addRenderable(Placemark.simple(launcher.getPosition(), new Color(1, 1, 1, 1), 4)); // for placement verification
+                        //symbolLayer.addRenderable(Placemark.simple(launcher.getPosition(), new Color(1, 1, 1, 1), 4)); // for placement verification
 
                         // "MIL-STD-2525 Friendly Heavy Machine Gun"
                         modifiers.clear();
@@ -97,7 +97,7 @@ public class PlacemarksMilStd2525Activity extends BasicGlobeActivity {
                             Position.fromDegrees(32.3902, 63.4161, 0),
                             "SFGPEWRH--MTUSG", modifiers);
                         symbolLayer.addRenderable(machineGun);
-                        symbolLayer.addRenderable(Placemark.simple(machineGun.getPosition(), new Color(1, 1, 1, 1), 4)); // for placement verification
+                        //symbolLayer.addRenderable(Placemark.simple(machineGun.getPosition(), new Color(1, 1, 1, 1), 4)); // for placement verification
 
                         // Signal a change in the WorldWind scene; requestRender() is callable from any thread.
                         getWorldWindow().requestRender();
