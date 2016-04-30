c0b664ab83772c3c4b953a37d70c26cdc5d4b88a
pdavidc <paul@paulcollinssoftware.com>
2016-04-29 21:17:09 -0700

    Added DrawContext support for vertex buffer object bindings

--- worldwind/src/main/java/gov/nasa/worldwind/render/DrawContext.java
+++ worldwind/src/main/java/gov/nasa/worldwind/render/DrawContext.java
@@ -9,6 +9,9 @@ import android.content.res.Resources;
 import android.graphics.Rect;
 import android.opengl.GLES20;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -87,6 +90,12 @@ public class DrawContext {
     protected int textureUnit = GLES20.GL_TEXTURE0;
 
     protected int[] textureId = new int[32];
+
+    protected int arrayBufferId;
+
+    protected int elementArrayBufferId;
+
+    protected int unitQuadBufferId;
 
     public DrawContext() {
     }
@@ -388,6 +397,9 @@ public class DrawContext {
         // Clear objects and values associated with the current OpenGL context.
         this.programId = 0;
         this.textureUnit = GLES20.GL_TEXTURE0;
+        this.arrayBufferId = 0;
+        this.elementArrayBufferId = 0;
+        this.unitQuadBufferId = 0;
         Arrays.fill(this.textureId, 0);
     }
 
@@ -471,5 +483,78 @@ public class DrawContext {
             this.textureId[textureUnitIndex] = textureId;
             GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
         }
+    }
+
+    /**
+     * Returns the name of the OpenGL buffer object bound to the specified target buffer.
+     *
+     * @param target the target buffer, either GL_ARRAY_BUFFER or GL_ELEMENT_ARRAY_BUFFER
+     *
+     * @return the currently bound buffer object, or 0 if no buffer object is bound
+     */
+    public int currentBuffer(int target) {
+        if (target == GLES20.GL_ARRAY_BUFFER) {
+            return this.arrayBufferId;
+        } else if (target == GLES20.GL_ELEMENT_ARRAY_BUFFER) {
+            return this.elementArrayBufferId;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Makes an OpenGL buffer object bound to a specified target buffer. This has no effect if the specified buffer
+     * object is already bound. The default is buffer 0, indicating that no buffer object is bound.
+     *
+     * @param target   the target buffer, either GL_ARRAY_BUFFER or GL_ELEMENT_ARRAY_BUFFER
+     * @param bufferId the name of the OpenGL buffer object to make active
+     */
+    public void bindBuffer(int target, int bufferId) {
+        if (target == GLES20.GL_ARRAY_BUFFER && this.arrayBufferId != bufferId) {
+            this.arrayBufferId = bufferId;
+            GLES20.glBindBuffer(target, bufferId);
+        } else if (target == GLES20.GL_ELEMENT_ARRAY_BUFFER && this.elementArrayBufferId != bufferId) {
+            this.elementArrayBufferId = bufferId;
+            GLES20.glBindBuffer(target, bufferId);
+        } else {
+            GLES20.glBindBuffer(target, bufferId);
+        }
+    }
+
+    /**
+     * Returns the name of an OpenGL buffer object containing a unit quadrilateral expressed as four vertices at (0, 1),
+     * (0, 0), (1, 1) and (1, 0). Each vertex is stored as two 32-bit floating point coordinates. The four vertices are
+     * in the order required by a triangle strip.
+     * <p/>
+     * The OpenGL buffer object is created on first use and cached. Subsequent calls to this method return the cached
+     * buffer object.
+     */
+    public int unitQuadBuffer() {
+        if (this.unitQuadBufferId != 0) {
+            return this.unitQuadBufferId;
+        }
+
+        int[] newBuffer = new int[1];
+        GLES20.glGenBuffers(1, newBuffer, 0);
+        this.unitQuadBufferId = newBuffer[0];
+
+        float[] points = new float[]{
+            0, 1,   // upper left corner
+            0, 0,   // lower left corner
+            1, 1,   // upper right corner
+            1, 0};  // lower right corner
+        int size = points.length;
+        FloatBuffer quadBuffer = ByteBuffer.allocateDirect(size * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
+        quadBuffer.put(points).rewind();
+
+        int currentBuffer = this.currentBuffer(GLES20.GL_ARRAY_BUFFER);
+        try {
+            this.bindBuffer(GLES20.GL_ARRAY_BUFFER, this.unitQuadBufferId);
+            GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, size * 4, quadBuffer, GLES20.GL_STATIC_DRAW);
+        } finally {
+            this.bindBuffer(GLES20.GL_ARRAY_BUFFER, currentBuffer);
+        }
+
+        return this.unitQuadBufferId;
     }
 }
