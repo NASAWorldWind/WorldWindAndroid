bef84a264a4553183c83781c7b6ffd9741801c37
Bruce Schubert <bruce@emxsys.com>
2016-05-10 11:01:28 -0700

    Removed stale comments and imports from PlacemarksStressTestActivity;

--- worldwind-examples/src/main/java/gov/nasa/worldwindx/PlacemarksStressTestActivity.java
+++ worldwind-examples/src/main/java/gov/nasa/worldwindx/PlacemarksStressTestActivity.java
@@ -13,7 +13,6 @@ import java.util.Random;
 import gov.nasa.worldwind.Navigator;
 import gov.nasa.worldwind.geom.Position;
 import gov.nasa.worldwind.layer.Layer;
-import gov.nasa.worldwind.layer.LayerList;
 import gov.nasa.worldwind.layer.RenderableLayer;
 import gov.nasa.worldwind.layer.ShowTessellationLayer;
 import gov.nasa.worldwind.render.ImageSource;
@@ -70,34 +69,9 @@ public class PlacemarksStressTestActivity extends BasicGlobeActivity implements 
         placemarksLayer.addRenderable(antiMeridian);
 
 
-        ////////////////////
-        // Stress Tests
-        ////////////////////
-
-        //Placemark.DEFAULT_EYE_DISTANCE_SCALING_THRESHOLD = 1e7;
-
         // Create a random number generator with an arbitrary seed
         // that will generate the same numbers between runs.
         Random random = new Random(123);
-
-
-//        // Create "sprinkles" -- random colored squares
-//        PlacemarkAttributes attributes = new PlacemarkAttributes();
-//        attributes.setImageScale(10);
-//        for (int i = 0; i < NUM_PLACEMARKS; i++) {
-//            // Generate a random color for this placemark
-//            attributes.setImageColor(Color.random());
-//            // Create an even distribution of latitude and longitudes
-//            // Use a random sin value to generate latitudes without clustering at the poles
-//            double lat = toDegrees(asin(random.nextDouble())) * (random.nextBoolean() ? 1 : -1);
-//            double lon = 180d - (random.nextDouble() * 360);
-//            Position pos = Position.fromDegrees(lat, lon, 0);
-//
-//            Placemark placemark = new Placemark(pos, new PlacemarkAttributes(attributes));
-//            placemark.setEyeDistanceScaling(false);
-//            placemarksLayer.addRenderable(placemark);
-//        }
-
 
         // Create pushpins anchored at the "pinpoints" with eye distance scaling
         PlacemarkAttributes attributes = PlacemarkAttributes.withImage(ImageSource.fromResource(R.drawable.pushpin_plain_red))
@@ -115,36 +89,6 @@ public class PlacemarksStressTestActivity extends BasicGlobeActivity implements 
 
             placemarksLayer.addRenderable(placemark);
         }
-
-
-//        // Create animated aircraft
-//        int aircraft = R.drawable.airplane;
-//        PlacemarkAttributes attributes = new PlacemarkAttributes();
-//        attributes.setImageOffset(PlacemarkAttributes.OFFSET_CENTER);
-//        attributes.setImageSource(aircraft);
-//        attributes.setImageScale(1);
-//
-//        for (int i = 0; i < NUM_PLACEMARKS; i++) {
-//
-//            // Use a random sin to generate the latitude to prevent clustering at the poles
-//            double lat = toDegrees(asin(random.nextDouble())) * (random.nextBoolean() ? 1 : -1);
-//            // Generate
-//            double lon = 180d - (random.nextDouble() * 360);
-//            Position pos = Position.fromDegrees(lat, lon, 0);
-//
-//            Double aircraftHeading = 90d; //new Double(WWMath.normalizeAngle180(random.nextDouble() * 360d));
-//            Placemark placemark = new Placemark(pos, new PlacemarkAttributes(attributes));
-//            placemark.setImageRotation(aircraftHeading);
-//            placemark.setImageRotationReference(WorldWind.RELATIVE_TO_GLOBE);
-//            //placemark.setImageTilt(90d);
-//            //placemark.setImageTiltReference(WorldWind.RELATIVE_TO_GLOBE);
-//            placemark.putUserProperty("aircraftHeading", aircraftHeading);
-//            placemarksLayer.addRenderable(placemark);
-//        }
-//
-//        // Set up an Android Handler to change the view
-//        this.animationHandler.postDelayed(this, DELAY_TIME);
-
     }
 
 
@@ -160,34 +104,6 @@ public class PlacemarksStressTestActivity extends BasicGlobeActivity implements 
         if (!this.pauseHandler) { // stop running when this activity is paused; the Handler is resumed in onResume
             this.animationHandler.postDelayed(this, 30);
         }
-
-//        // Aircraft animation
-//        this.getWorldWindow().queueEvent(new Runnable() {
-//            @Override
-//            public void run() {
-//                LayerList layers = getWorldWindow().getLayers();
-//                int index = layers.indexOfLayerNamed("Placemarks");
-//                RenderableLayer layer = (RenderableLayer) layers.getLayer(index);
-//                Iterator<Renderable> i = layer.iterator();
-//                while (i.hasNext()) {
-//                    Renderable r = i.next();
-//                    if (r instanceof Placemark) {
-//                        Placemark placemark = (Placemark) r;
-//                        Double currentHeading = (Double) placemark.getUserProperty("aircraftHeading");
-//                        if (currentHeading != null) {
-//                            Position pos = placemark.getPosition();
-//                            Location endPoint = pos.rhumbLocation(currentHeading, 0.001, new Location());
-//                            Location midPoint = pos.interpolateAlongPath(endPoint, WorldWind.RHUMB_LINE, 0.5, new Location());
-//                            //double newHeading = midPoint.greatCircleAzimuth(endPoint);
-//                            placemark.setPosition(pos.set(midPoint.latitude, midPoint.longitude, pos.altitude));
-//                            //placemark.putUserProperty("aircraftHeading", new Double(newHeading));
-//                        }
-//                    }
-//                }
-//
-//            }
-//        });
-
     }
 
     @Override
