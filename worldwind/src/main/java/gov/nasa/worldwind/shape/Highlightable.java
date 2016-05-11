e816100be9d9d3b032000df3a86af4d907f854b9
Bruce Schubert <bruce@emxsys.com>
2016-05-11 13:04:12 -0700

    Added Highlightable interface to shape package.

---
+++ worldwind/src/main/java/gov/nasa/worldwind/shape/Highlightable.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration. All Rights Reserved.
+ */
+
+package gov.nasa.worldwind.shape;
+
+/**
+ * An interface to control a shape's highlighting. Shapes implementing this interface have their own
+ * highlighting behaviors and attributes and the means for setting them.
+ */
+
+public interface Highlightable {
+
+    /**
+     * Indicates whether to highlight the shape.
+     *
+     * @return true to highlight the shape, otherwise false.
+     */
+    boolean isHighlighted();
+
+    /**
+     * Specifies whether to highlight the shape.
+     *
+     * @param highlighted true to highlight the shape, otherwise false.
+     */
+    void setHighlighted(boolean highlighted);
+}
