738ed3a94961b8238299cc5afe6d72be9d07f751
Bruce Schubert <bruce@emxsys.com>
2016-05-13 09:58:58 -0700

    Refactored NavigationEventActivity to demonstrate detection and consumption of programmatic and user input events. - overlay colors change on NAVIGATOR_STOPPED - user events generate inertia - inertia generates programmatic NAVIGATOR_MOVED events - improved UI with animation fade of the crosshairs

--- worldwind-examples/src/main/java/gov/nasa/worldwindx/NavigatorEventActivity.java
+++ worldwind-examples/src/main/java/gov/nasa/worldwindx/NavigatorEventActivity.java
@@ -5,49 +5,93 @@
 
 package gov.nasa.worldwindx;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Choreographer;
+import android.view.InputEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import gov.nasa.worldwind.Navigator;
 import gov.nasa.worldwind.NavigatorEvent;
 import gov.nasa.worldwind.NavigatorListener;
 import gov.nasa.worldwind.WorldWind;
 import gov.nasa.worldwind.WorldWindow;
 import gov.nasa.worldwind.geom.Camera;
+import gov.nasa.worldwind.geom.Location;
 import gov.nasa.worldwind.geom.LookAt;
 
-public class NavigatorEventActivity extends BasicGlobeActivity {
+public class NavigatorEventActivity extends BasicGlobeActivity implements Choreographer.FrameCallback {
 
+    // UI elements
     protected TextView latView;
 
     protected TextView lonView;
 
     protected TextView altView;
 
+    protected ImageView crosshairs;
+
+    protected ViewGroup overlay;
+
+    // Use pre-allocated navigator state objects to avoid per-event memory allocations
     private LookAt lookAt = new LookAt();
 
     private Camera camera = new Camera();
 
-    private double[] dms = new double[3];
+    // Track the navigation event time so the overlay refresh rate can be throttled
+    private long lastEventTime;
 
-    private long lastEventTime = 0;
+    // Animation object used to fade the overlays
+    private AnimatorSet animatorSet;
+
+    private boolean crosshairsActive;
+
+    // Globe rotation onFrame animation settings
+    private Location currentLocation = new Location();
+
+    private Location targetLocation = new Location();
+
+    private Location lastLocation; // lazily allocated
+
+    private double radiansPerMillisecond;
+
+    private double azimuth;
+
+    private final double COAST_DURATION_MILLIS = 3000;
+
+    private double coastTimeRemainingMillis;
+
+    private long lastFrameTimeNanos;
+
+    private boolean coasting;
+
+    // Track the state of this activity to start/stope the globe animation
+    private boolean activityPaused;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setAboutBoxTitle("About the " + getResources().getText(R.string.title_navigator_event));
-        setAboutBoxText("Demonstrates how to receive notification of navigator events.");
+        setAboutBoxText("Demonstrates how to receive and consume navigator events.\n\n"
+            + "The crosshairs and overlays react to the user input");
 
-        // Enable the status bar and crosshairs (their default visibility is 'gone')
-        findViewById(R.id.globe_status).setVisibility(View.VISIBLE);
-        findViewById(R.id.globe_crosshairs).setVisibility(View.VISIBLE);
-
-        // Initialize the text views used to display the Navigator state
+        // Initialize the UI elements that we'll update upon the navigation events
+        this.crosshairs = (ImageView) findViewById(R.id.globe_crosshairs);
+        this.overlay = (ViewGroup) findViewById(R.id.globe_status);
+        this.crosshairs.setVisibility(View.VISIBLE);
+        this.overlay.setVisibility(View.VISIBLE);
         this.latView = (TextView) findViewById(R.id.lat_value);
         this.lonView = (TextView) findViewById(R.id.lon_value);
         this.altView = (TextView) findViewById(R.id.alt_value);
-
+        ObjectAnimator fadeOut = ObjectAnimator.ofFloat(this.crosshairs, "alpha", 0f).setDuration(1500);
+        fadeOut.setStartDelay(500);
+        this.animatorSet = new AnimatorSet();
+        this.animatorSet.play(fadeOut);
 
         // Create a simple Navigator Listener that logs navigator events emitted by the World Window.
         NavigatorListener listener = new NavigatorListener() {
@@ -56,23 +100,35 @@ public class NavigatorEventActivity extends BasicGlobeActivity {
 
                 long currentTime = System.currentTimeMillis();
                 long elapsedTime = currentTime - lastEventTime;
+                int eventAction = event.getAction();
+                InputEvent lastInputEvent = event.getLastInputEvent();
+                boolean receivedUserInput = (eventAction == WorldWind.NAVIGATOR_MOVED && lastInputEvent != null);
 
-                // Update the status overlay whenever the navigator stops moving,
-                // otherwise at an arbitrary maximum refresh rate of 20 Hz.
-                if (event.getAction() == WorldWind.NAVIGATOR_STOPPED || elapsedTime > 50) {
-                    // Get the current navigator state
+                // Update the status overlay views whenever the navigator stops moving,
+                // and also it is moving but at an (arbitrary) maximum refresh rate of 20 Hz.
+                if (eventAction == WorldWind.NAVIGATOR_STOPPED || elapsedTime > 50) {
+
+                    // Get the current navigator state to apply to the overlays
                     event.getNavigator().getAsLookAt(wwd.getGlobe(), lookAt);
                     event.getNavigator().getAsCamera(wwd.getGlobe(), camera);
 
-                    // Update the status overlay from the navigator state
-                    latView.setText(formatLatitude(lookAt.latitude));
-                    lonView.setText(formatLongitude(lookAt.longitude));
-                    altView.setText(formatAltitude(camera.altitude));
+                    // Update the overlays
+                    updateOverlayContents(lookAt, camera);
+                    updateOverlayColor(eventAction, lastInputEvent);
 
-                    // Set the text color based on the 'moved' vs 'stopped' action
-                    setTextColor(event.getAction());
+                    // Apply some inertial to the user's gesture
+                    if (receivedUserInput) {
+                        updateInertiaSettings(lookAt, elapsedTime);
+                    }
 
                     lastEventTime = currentTime;
+                }
+
+                // Show the crosshairs while the user is gesturing and fade them out after the user stops
+                if (receivedUserInput) {
+                    showCrosshairs();
+                } else {
+                    fadeCrosshairs();
                 }
             }
         };
@@ -81,64 +137,153 @@ public class NavigatorEventActivity extends BasicGlobeActivity {
         this.getWorldWindow().addNavigatorListener(listener);
     }
 
-    private void setTextColor(@WorldWind.NavigatorAction int action) {
-        int color = Color.WHITE;
-        if (action == WorldWind.NAVIGATOR_MOVED) {
-            color = Color.RED;
-        } else if (action == WorldWind.NAVIGATOR_STOPPED) {
-            color = Color.YELLOW;
+    /**
+     * Animates a rotating globe.
+     */
+    @Override
+    public void doFrame(long frameTimeNanos) {
+        if (this.lastFrameTimeNanos != 0) {
+            // Compute the frame duration in milliseconds.
+            double frameDurationMillis = (frameTimeNanos - this.lastFrameTimeNanos) * 1.0e-6;
+
+            // Move the navigator to simulate inertia from the user's last move gesture
+            if (this.coastTimeRemainingMillis > 0) {
+                Navigator navigator = getWorldWindow().getNavigator();
+
+                // Compute the distance to move in this frame
+                double distanceRadians = this.radiansPerMillisecond * frameDurationMillis;
+                this.currentLocation.set(navigator.getLatitude(), navigator.getLongitude());
+                this.currentLocation.greatCircleLocation(this.azimuth, distanceRadians, this.targetLocation);
+
+                navigator.setLatitude(this.targetLocation.latitude);
+                navigator.setLongitude(this.targetLocation.longitude);
+
+                // Dampen the inertia
+                this.coastTimeRemainingMillis -= frameDurationMillis;
+                if (this.coastTimeRemainingMillis > 0) {
+                    this.radiansPerMillisecond *= coastTimeRemainingMillis / COAST_DURATION_MILLIS;
+                }
+
+                // Redraw the World Window to display the above changes.
+                this.getWorldWindow().requestRedraw();
+            }
         }
+
+        if (!this.activityPaused) { // stop animating when this Activity is activityPaused
+            Choreographer.getInstance().postFrameCallback(this);
+        }
+
+        this.lastFrameTimeNanos = frameTimeNanos;
+    }
+
+    /**
+     * Pauses the globe animation.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Stop running the globe rotation animation when this activity is activityPaused.
+        this.activityPaused = true;
+        this.lastFrameTimeNanos = 0;
+    }
+
+    /**
+     * Starts the globe animation.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Resume the globe rotation animation
+        this.activityPaused = false;
+        this.lastFrameTimeNanos = 0;
+        Choreographer.getInstance().postFrameCallback(this);
+    }
+
+    /**
+     * Updates the settings for user gesture inertia.
+     *
+     * @param lookAt        The current lookAt
+     * @param elapsedTimeMs The time elapsed since the last user action
+     */
+    protected void updateInertiaSettings(LookAt lookAt, long elapsedTimeMs) {
+        if (this.lastLocation == null) {
+            this.lastLocation = new Location(lookAt.latitude, lookAt.longitude);
+        }
+        // Compute the direction used for the coasting inertia
+        this.currentLocation.set(lookAt.latitude, lookAt.longitude);
+        this.azimuth = this.lastLocation.greatCircleAzimuth(this.currentLocation);
+
+        // Compute the velocity used for the coasting
+        this.radiansPerMillisecond = this.lastLocation.greatCircleDistance(this.currentLocation) / elapsedTimeMs;
+
+        // Reset the coasting period on each user action
+        this.coastTimeRemainingMillis = COAST_DURATION_MILLIS;
+
+        this.lastLocation.set(this.currentLocation);
+    }
+
+    /**
+     * Makes the crosshairs visible.
+     */
+    protected void showCrosshairs() {
+        if (this.animatorSet.isStarted()) {
+            this.animatorSet.cancel();
+        }
+        this.crosshairs.setAlpha(1.0f);
+        this.crosshairsActive = true;
+    }
+
+    /**
+     * Fades the crosshairs using animation.
+     */
+    protected void fadeCrosshairs() {
+        if (this.crosshairsActive) {
+            this.crosshairsActive = false;
+            if (!this.animatorSet.isStarted()) {
+                this.animatorSet.start();
+            }
+        }
+    }
+
+    /**
+     * Displays navigator state information in the status overlay views.
+     *
+     * @param lookAt Where the navigator is looking
+     * @param camera Where the camera is positioned
+     */
+    protected void updateOverlayContents(LookAt lookAt, Camera camera) {
+        latView.setText(formatLatitude(lookAt.latitude));
+        lonView.setText(formatLongitude(lookAt.longitude));
+        altView.setText(formatAltitude(camera.altitude));
+    }
+
+    /**
+     * Brightens the colors of the overlay views when when user input occurs.
+     *
+     * @param eventAction    The action associated with this navigator event
+     * @param lastInputEvent The last user input event; will be null if no user input was detected during this navigator
+     *                       event
+     */
+    protected void updateOverlayColor(@WorldWind.NavigatorAction int eventAction, InputEvent lastInputEvent) {
+        int color = (eventAction == WorldWind.NAVIGATOR_STOPPED) ? 0xA0FFFF00 /*semi-transparent yellow*/ : Color.YELLOW;
         latView.setTextColor(color);
         lonView.setTextColor(color);
         altView.setTextColor(color);
     }
 
-    private String formatLatitude(double latitude) {
-        angleToDms(latitude, dms);
-        int sign = (int) Math.signum(dms[0]);
-        return String.format("%2d\u00B0 %02d\' %04.1f\"%s",
-            (int) dms[0],
-            (int) dms[1],
-            dms[2],
-            (sign >= 0.0 ? "N" : "S"));
+    protected String formatLatitude(double latitude) {
+        int sign = (int) Math.signum(latitude);
+        return String.format("%6.3f°%s", (latitude * sign), (sign >= 0.0 ? "N" : "S"));
     }
 
-    private String formatLongitude(double longitude) {
-        angleToDms(longitude, dms);
+    protected String formatLongitude(double longitude) {
         int sign = (int) Math.signum(longitude);
-        return String.format("%3d\u00B0 %02d\' %04.1f\"%s",
-            (int) dms[0],
-            (int) dms[1],
-            dms[2],
-            (sign >= 0.0 ? "E" : "W"));
+        return String.format("%7.3f°%s", (longitude * sign), (sign >= 0.0 ? "E" : "W"));
     }
 
-    private String formatAltitude(double altitude) {
+    protected String formatAltitude(double altitude) {
         return String.format("Eye: %,.0f %s",
-            altitude < 100000 ? altitude : altitude / 1000,
-            altitude < 100000 ? "m" : "km");
+            (altitude < 100000 ? altitude : altitude / 1000),
+            (altitude < 100000 ? "m" : "km"));
     }
-
-    private static double[] angleToDms(double angle, double[] dms) {
-        double temp = Math.abs(angle);
-        int d = (int) Math.floor(temp);
-        temp = (temp - d) * 60d;
-        int m = (int) Math.floor(temp);
-        temp = (temp - m) * 60d;
-        double s = temp;
-        if (s == 60) {
-            m++;
-            s = 0;
-        } // Fix rounding errors
-        if (m == 60) {
-            d++;
-            m = 0;
-        }
-        dms[0] = d;
-        dms[1] = m;
-        dms[2] = s;
-
-        return dms;
-    }
-
 }
