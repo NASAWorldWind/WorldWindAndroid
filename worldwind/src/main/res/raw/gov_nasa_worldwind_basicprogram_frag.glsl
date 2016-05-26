c3e3139ee9ec25a76531f4835dfc08402fb8daa5
pdavidc <paul@paulcollinssoftware.com>
2016-05-26 14:36:53 -0700

    Added pick support to BasicShaderProgram

--- worldwind/src/main/res/raw/gov_nasa_worldwind_basicprogram_frag.glsl
+++ worldwind/src/main/res/raw/gov_nasa_worldwind_basicprogram_frag.glsl
@@ -5,6 +5,7 @@
 
 precision mediump float;
 
+uniform bool enablePickMode;
 uniform bool enableTexture;
 uniform vec4 color;
 uniform sampler2D texSampler;
@@ -12,9 +13,13 @@ uniform sampler2D texSampler;
 varying vec2 texCoord;
 
 void main() {
-    if (enableTexture) {
-        /* Modulate the specified fragment color by the specified 2D texture's color */
-        gl_FragColor = color * texture2D(texSampler, texCoord);
+    if (enablePickMode && enableTexture) {
+        /* Return the specified fragment RGB color with the texture's Alpha component */
+        float texMask = floor(texture2D(texSampler, texCoord).a + 0.5);
+        gl_FragColor = color * texMask;
+    } else if (!enablePickMode && enableTexture) {
+       /* Modulate the specified fragment color by the specified 2D texture's color */
+       gl_FragColor = color * texture2D(texSampler, texCoord);
     } else {
         /* Return the specified fragment color */
         gl_FragColor = color;
