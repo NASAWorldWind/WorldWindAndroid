584beaae555ec5f91e76b5118a0967fd41043061
pdavidc <paul@paulcollinssoftware.com>
2016-05-30 09:53:30 -0700

    Updated DrawableSurfaceTexture to support color and picking

--- worldwind/src/main/res/raw/gov_nasa_worldwind_surfacetextureprogram_vert.glsl
+++ worldwind/src/main/res/raw/gov_nasa_worldwind_surfacetextureprogram_vert.glsl
@@ -5,6 +5,7 @@
 
 precision mediump float;
 
+uniform bool enableTexture;
 uniform mat4 mvpMatrix;
 uniform mat3 texCoordMatrix[2];
 
@@ -18,7 +19,9 @@ void main() {
     gl_Position = mvpMatrix * vertexPoint;
 
     /* Transform the vertex tex coord by the tex coord matrices. */
-    vec3 texCoord3 = vec3(vertexTexCoord, 1.0);
-    texCoord[0] = (texCoordMatrix[0] * texCoord3).st;
-    texCoord[1] = (texCoordMatrix[1] * texCoord3).st;
+    if (enableTexture) {
+        vec3 texCoord3 = vec3(vertexTexCoord, 1.0);
+        texCoord[0] = (texCoordMatrix[0] * texCoord3).st;
+        texCoord[1] = (texCoordMatrix[1] * texCoord3).st;
+    }
 }
