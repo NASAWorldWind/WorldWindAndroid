f7adf620e9952d7fcfd253a1d5f5cb594d771328
Bruce Schubert <bruce@emxsys.com>
2016-06-10 16:28:59 -0700

    Updated SurfaceImage with a Movable interface implementation

--- worldwind/src/main/java/gov/nasa/worldwind/shape/SurfaceImage.java
+++ worldwind/src/main/java/gov/nasa/worldwind/shape/SurfaceImage.java
@@ -5,9 +5,14 @@
 
 package gov.nasa.worldwind.shape;
 
+import java.util.Arrays;
+
 import gov.nasa.worldwind.PickedObject;
 import gov.nasa.worldwind.draw.DrawableSurfaceTexture;
+import gov.nasa.worldwind.geom.Location;
+import gov.nasa.worldwind.geom.Position;
 import gov.nasa.worldwind.geom.Sector;
+import gov.nasa.worldwind.globe.Globe;
 import gov.nasa.worldwind.render.AbstractRenderable;
 import gov.nasa.worldwind.render.ImageSource;
 import gov.nasa.worldwind.render.RenderContext;
@@ -16,7 +21,7 @@ import gov.nasa.worldwind.render.Texture;
 import gov.nasa.worldwind.util.Logger;
 import gov.nasa.worldwind.util.Pool;
 
-public class SurfaceImage extends AbstractRenderable {
+public class SurfaceImage extends AbstractRenderable implements Movable {
 
     protected final Sector sector = new Sector();
 
@@ -103,4 +108,94 @@ public class SurfaceImage extends AbstractRenderable {
 
         return program;
     }
+
+    /**
+     * A position associated with the object that indicates its aggregate geographic position. The chosen position
+     * varies among implementers of this interface. For objects defined by a list of positions, the reference position
+     * is typically the first position in the list. For symmetric objects the reference position is often the center of
+     * the object. In many cases the object's reference position may be explicitly specified by the application.
+     *
+     * @return the object's reference position, or null if no reference position is available.
+     */
+    @Override
+    public Position getReferencePosition() {
+        Position refPosition = new Position(this.sector.centroidLatitude(), this.sector.centroidLongitude(), 0);
+        return refPosition;
+    }
+
+    /**
+     * Move the shape over the globe's surface while maintaining its original azimuth, its orientation relative to
+     * North.
+     *
+     * @param globe    the globe on which to move the shape.
+     * @param position the new position of the shape's reference position.
+     */
+    @Override
+    public void moveTo(Globe globe, Position position) {
+        Position oldRef = this.getReferencePosition();
+        if (oldRef == null)
+            return;
+
+        Location swCorner = new Location(this.sector.minLatitude(), this.sector.minLongitude());
+        Location nwCorner = new Location(this.sector.maxLatitude(), this.sector.minLongitude());
+        Location seCorner = new Location(this.sector.minLatitude(), this.sector.maxLongitude());
+        Location neCorner = new Location(this.sector.maxLatitude(), this.sector.maxLongitude());
+
+        final double EAST = 90;
+        final double WEST = -90;
+        final double NORTH = 0;
+        final double SOUTH = 180;
+
+        // Determine the delta from the reference point to sector's anchor (SW corner)
+        double azimuthDegrees = oldRef.greatCircleAzimuth(swCorner);
+        double distanceRadians = oldRef.greatCircleDistance(swCorner);
+        // Determine the width and height of the sector
+        double widthRadians = swCorner.rhumbDistance(seCorner);
+        double heightRadians = swCorner.rhumbDistance(nwCorner);
+        double dLat = nwCorner.latitude - swCorner.latitude;
+
+        // Compute a new position for the SW corner
+        position.greatCircleLocation(azimuthDegrees, distanceRadians, swCorner);
+
+        // If the dragged image would span the pole then constrain to the pole
+        if (swCorner.latitude + dLat > 90) {
+            nwCorner.set(90, swCorner.longitude);
+            //swCorner.set(nwCorner.latitude - dLat, swCorner.longitude);
+            nwCorner.rhumbLocation(SOUTH, heightRadians, swCorner);
+        } else {
+            // Compute the NW corner with the original height
+            nwCorner.set(swCorner.latitude + dLat, swCorner.longitude);
+            swCorner.rhumbLocation(NORTH, heightRadians, nwCorner);
+        }
+
+        // Compute the SE corner, using the original width
+        swCorner.rhumbLocation(EAST, widthRadians, seCorner);
+
+
+        // If the dragged image would span the dateline then snap the image the other side
+        if (Location.locationsCrossAntimeridian(Arrays.asList(new Location[]{swCorner, seCorner}))) {
+            // TODO: create JIRA issue regarding Sector Anti-meridian limitation
+            // There's presently no support for placing SurfaceImages crossing the Anti-meridian
+            // Snap the image to the other side of the date line
+            double dragAzimuth = oldRef.greatCircleAzimuth(position);
+            if (dragAzimuth < 0) {
+                // Set the East edge of the sector to the dateline
+                seCorner.set(seCorner.latitude, 180);
+                seCorner.rhumbLocation(WEST, widthRadians, swCorner);
+            } else {
+                // Set the West edge of the sector to the dateline
+                swCorner.set(swCorner.latitude, -180);
+                swCorner.rhumbLocation(EAST, widthRadians, seCorner);
+            }
+        }
+
+        // Compute the delta lon values from the new SW position
+        double dLon = seCorner.longitude - swCorner.longitude;
+
+        // Update the image's sector to move the image
+        if (dLat > 0 && dLon > 0) {
+            this.sector.set(swCorner.latitude, swCorner.longitude, dLat, dLon);
+        }
+    }
+
 }
