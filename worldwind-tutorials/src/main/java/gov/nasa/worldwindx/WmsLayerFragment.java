e49d92b0a6f62bc08b2de7d16c26593d5132ca1e
Bruce Schubert <bruce@emxsys.com>
2016-08-04 14:39:21 -0700

    Changed WMS Layer tutorial's map layer. - The imagery returned for Sea Surface Temperature recently seems to have an anomoly in its color palette. - Switched to Land Surface Temperature.

--- worldwind-tutorials/src/main/java/gov/nasa/worldwindx/WmsLayerFragment.java
+++ worldwind-tutorials/src/main/java/gov/nasa/worldwindx/WmsLayerFragment.java
@@ -38,7 +38,7 @@ public class WmsLayerFragment extends BasicGlobeFragment {
         WmsLayerConfig config = new WmsLayerConfig();
         config.serviceAddress = "http://neowms.sci.gsfc.nasa.gov/wms/wms";
         config.wmsVersion = "1.1.1"; // NEO server works best with WMS 1.1.1
-        config.layerNames = "MYD28M"; // Sea surface temperature (MODIS)
+        config.layerNames = "MOD_LSTD_CLIM_M"; // Land Surface Temperature
         WmsLayer layer = new WmsLayer(new Sector().setFullSphere(), 1e3, config); // 1km resolution
 
         // Add the WMS layer to the World Window.
