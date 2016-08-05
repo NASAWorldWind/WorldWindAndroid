f124f64c5b554619acbd9c678b1375e4a3ec7315
pdavidc <paul@paulcollinssoftware.com>
2016-08-05 08:42:28 -0700

    Updated Paths tutorial inline documentation

--- worldwind-tutorials/src/main/java/gov/nasa/worldwindx/PathsActivity.java
+++ worldwind-tutorials/src/main/java/gov/nasa/worldwindx/PathsActivity.java
@@ -39,15 +39,15 @@ public class PathsActivity extends BasicGlobeActivity {
         Path path = new Path(positions);
         layer.addRenderable(path);
 
-        // Create a basic path with the default attributes, the CLAMP_TO_GROUND altitude mode,
-        // and the default path type (GREAT_CIRCLE).
+        // Create a terrain following path with the default attributes, and the default path type (GREAT_CIRCLE).
         positions = Arrays.asList(
             Position.fromDegrees(40, -180, 0),
             Position.fromDegrees(20, -100, 0),
             Position.fromDegrees(40, -40, 0)
         );
         path = new Path(positions);
-        path.setAltitudeMode(WorldWind.CLAMP_TO_GROUND);
+        path.setAltitudeMode(WorldWind.CLAMP_TO_GROUND); // clamp the path vertices to the ground
+        //path.setFollowTerrain(true); // follow the ground between path vertices
         layer.addRenderable(path);
 
         // Create an extruded path with the default attributes, the default altitude mode (ABSOLUTE),
