38e9ae6e5d8c3afe00296ff59b120a704e7d4cd1
Bruce Schubert <bruce@emxsys.com>
2016-09-30 14:25:12 -0700

    Updated the Labels Tutorial. - Updated content for the LabelsFragment - New content for the html

--- worldwind-tutorials/src/main/java/gov/nasa/worldwindx/LabelsFragment.java
+++ worldwind-tutorials/src/main/java/gov/nasa/worldwindx/LabelsFragment.java
@@ -7,8 +7,6 @@ package gov.nasa.worldwindx;
 
 import android.graphics.Typeface;
 
-import org.w3c.dom.Text;
-
 import gov.nasa.worldwind.WorldWind;
 import gov.nasa.worldwind.WorldWindow;
 import gov.nasa.worldwind.geom.LookAt;
@@ -17,13 +15,12 @@ import gov.nasa.worldwind.geom.Position;
 import gov.nasa.worldwind.layer.RenderableLayer;
 import gov.nasa.worldwind.render.Color;
 import gov.nasa.worldwind.shape.Label;
-import gov.nasa.worldwind.shape.Placemark;
 import gov.nasa.worldwind.shape.TextAttributes;
 
 public class LabelsFragment extends BasicGlobeFragment {
 
     /**
-     * Creates a new WorldWindow with a RenderableLayer populated with four Labels.
+     * Creates a new WorldWindow with a RenderableLayer populated with several Labels.
      */
     @Override
     public WorldWindow createWorldWindow() {
@@ -34,55 +31,77 @@ public class LabelsFragment extends BasicGlobeFragment {
         RenderableLayer layer = new RenderableLayer("Renderables");
         wwd.getLayers().addLayer(layer);
 
-        // Create a simple label with the default text attributes
-        Label ventura = new Label(new Position(34.281, -119.293, 0), "Ventura");
-        layer.addRenderable(ventura);
+        // Create a simple label with the default text attributes:
+        //  text size is 24,
+        //  text alignment is bottom-center,
+        //  text color is white.
+        Label sanNicolas = new Label(new Position(33.262, -119.538, 0),
+            "San Nicolas");
+        layer.addRenderable(sanNicolas);
 
-        // Create a big yellow label from a text attributes bundle
-        TextAttributes textAttributes = new TextAttributes()
+        // Define the text attributes for the Nat'l Park Service
+        // using a big, bold, italic serif font.
+        TextAttributes parkAttributes = new TextAttributes()
             .setTypeface(Typeface.create("serif", Typeface.BOLD_ITALIC))
-            .setTextColor(new Color(1f, 1f, 0f, 1f))// yellow, opaque
-            .setTextSize(50f);                      // default size is 24
-        Label island = new Label(new Position(34.005, -119.392, 0),
-            "Anacapa Island", textAttributes);
-        layer.addRenderable(island);
+            .setTextColor(new Color(0f, 1f, 0f, 0.5f))  // green, 50% opacity
+            .setTextSize(50f);
 
-        // Create a collection of labels that demonstrate
-        // label offsets from a given position.
-        final Position pos = new Position(34.2, -119.5, 0);
-        Offset northEast = new Offset(
-            WorldWind.OFFSET_PIXELS, -40,       // move left-edge right
-            WorldWind.OFFSET_PIXELS, -40);      // move lower-edge up
-        Offset northWest = new Offset(
-            WorldWind.OFFSET_INSET_PIXELS, -40, // move right-edge left
-            WorldWind.OFFSET_PIXELS, -40);      // move lower-edge up
-        Offset southWest = new Offset(
-            WorldWind.OFFSET_INSET_PIXELS, -40, // move right-edge left
-            WorldWind.OFFSET_INSET_PIXELS, -40);// move top-edge down
-        Offset southEast = new Offset(
-            WorldWind.OFFSET_PIXELS, -40,       // move left-edge right
-            WorldWind.OFFSET_INSET_PIXELS, -40);// move top-edge down
+        // Create a two-line label for the Nat'l Park, the top
+        // label's bottom-center is aligned to the position,
+        // the bottom label's top-center is aligned to the position.
+        Label park1 = new Label(new Position(33.9, -119.7, 0),
+            "Channel Islands", parkAttributes);
 
-        Label label1 = new Label(pos, "NW: " + northWest + " _", new TextAttributes().setTextOffset(northWest));
-        Label label2 = new Label(pos, "SW: " + southWest + " ¯", new TextAttributes().setTextOffset(southWest));
-        Label label3 = new Label(pos, "_ NE: " + northEast, new TextAttributes().setTextOffset(northEast));
-        Label label4 = new Label(pos, "¯ SE: " + southEast, new TextAttributes().setTextOffset(southEast));
-        Label label5 = new Label(pos, "default");   // anchor point is bottomCenter of label
+        Label park2 = new Label(new Position(33.9, -119.7, 0),
+            "National Park",
+            new TextAttributes(parkAttributes)
+                .setTextOffset(Offset.topCenter()));
+        layer.addRenderable(park1);
+        layer.addRenderable(park2);
 
-        layer.addRenderable(label1);
-        layer.addRenderable(label2);
-        layer.addRenderable(label3);
-        layer.addRenderable(label4);
-        layer.addRenderable(label5);
-        layer.addRenderable(
-            Placemark.createWithColorAndSize(pos,
-                new Color(android.graphics.Color.YELLOW), 10));
+        // Define the text attributes used for the Nat'l Park islands
+        // with a yellow, bold san-serif font
+        TextAttributes islandAttributes = new TextAttributes()
+            .setTypeface(Typeface.create("san-serif", Typeface.BOLD))
+            .setTextColor(new Color(1f, 1f, 0f, 1f));// yellow, opaque
 
+        // Create a labels for Santa Cruz and San Miguel using
+        // the default bottom-center text alignment, but make
+        // the text size larger than the default size (24).
+        Label santaCruz = new Label(new Position(34.04, -119.77, 0),
+            "Santa Cruz", new TextAttributes(islandAttributes)
+                .setTextSize(40f));
+        layer.addRenderable(santaCruz);
 
-        // And finally, for this demo, position the viewer to look at the airport placemark
-        // from a tilted perspective when this Android activity is created.
+        Label sanMiguel = new Label(new Position(34.06, -120.37, 0),
+            "Santa Miguel", new TextAttributes(islandAttributes)
+                .setTextSize(32f));
+        layer.addRenderable(sanMiguel);
+
+        // Create a label for Santa Rosa where the top-right
+        // of the text is aligned with the island's position.
+        Label santaRosa = new Label(new Position(33.95, -120.15, 0),
+            "Santa Rosa", new TextAttributes(islandAttributes)
+                .setTextSize(40f)
+                .setTextOffset(Offset.topRight()));
+        layer.addRenderable(santaRosa);
+
+        // Create smaller labels for Anacapa and Santa Barbara where
+        // the bottom-left is aligned with the island's position.
+        Label anacapa = new Label(new Position(34.005, -119.392, 0),
+            "Anacapa", new TextAttributes(islandAttributes)
+                .setTextSize(28f)
+                .setTextOffset(Offset.bottomLeft()));
+        layer.addRenderable(anacapa);
+
+        Label santaBarbara = new Label(new Position(33.475, -119.037, 0),
+            "Santa Barbara", anacapa.getAttributes());
+        layer.addRenderable(santaBarbara);
+
+        // Position the viewer to look down at the Channel Islands.
+        final Position pos = new Position(33.7, -119.6, 0);
         LookAt lookAt = new LookAt().set(pos.latitude, pos.longitude, pos.altitude, WorldWind.ABSOLUTE,
-            1e5 /*range*/, 0 /*heading*/, 0 /*tilt*/, 0 /*roll*/);
+            30e4 /*range*/, 0 /*heading*/, 0 /*tilt*/, 0 /*roll*/);
         wwd.getNavigator().setAsLookAt(wwd.getGlobe(), lookAt);
 
         return wwd;
