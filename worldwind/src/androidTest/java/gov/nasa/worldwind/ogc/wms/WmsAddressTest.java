dac08a4af059ae131680e3c1378e00ed62e6dcbf
Zach Glueckert <zachary.r.glueckert@nasa.gov>
2016-12-22 16:02:37 -0500

    Add WmsAddress parse and read test

---
+++ worldwind/src/androidTest/java/gov/nasa/worldwind/ogc/wms/WmsAddressTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration. All Rights Reserved.
+ */
+
+package gov.nasa.worldwind.ogc.wms;
+
+import android.content.res.Resources;
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.namespace.QName;
+
+import gov.nasa.worldwind.R;
+import gov.nasa.worldwind.util.xml.XmlElementModel;
+import gov.nasa.worldwind.util.xml.XmlPullParserContext;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class WmsAddressTest {
+
+    @Test
+    public void testAddress_ParseAndReadSampleWms() throws XmlPullParserException, IOException {
+
+        // Initialize the context and basic model
+        XmlPullParserContext context = new XmlPullParserContext(XmlPullParserContext.DEFAULT_NAMESPACE);
+        Resources resources = getInstrumentation().getTargetContext().getResources();
+        InputStream is = resources.openRawResource(R.raw.gov_nasa_worldwind_sample_wms_xml);
+        context.setParserInput(is);
+        context.registerParsableModel(
+            new QName(XmlPullParserContext.DEFAULT_NAMESPACE, "ContactAddress"),
+            new WmsAddress(XmlPullParserContext.DEFAULT_NAMESPACE));
+
+        XmlElementModel elementModel = new XmlElementModel(XmlPullParserContext.DEFAULT_NAMESPACE);
+        Object o;
+        do {
+            o = elementModel.read(context);
+        } while (o != null);
+        WmsAddress wmsAddress = (WmsAddress) ((XmlElementModel) ((XmlElementModel) elementModel.getField("Service"))
+            .getField("ContactInformation")).getField("ContactAddress");
+
+        assertEquals("test address type", "Business", wmsAddress.getAddressType());
+        assertEquals("test address", "1234 Hollywood Lane", wmsAddress.getAddress());
+        assertEquals("test city", "Springfield", wmsAddress.getCity());
+        assertEquals("test state", "Unknown", wmsAddress.getStateOrProvince());
+        assertEquals("test postal code", "90210", wmsAddress.getPostCode());
+        assertEquals("test country", "United States of America", wmsAddress.getCountry());
+
+    }
+
+}
---
+++ worldwind/src/main/java/gov/nasa/worldwind/ogc/wms/WmsAddress.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2016 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration. All Rights Reserved.
+ */
+
+package gov.nasa.worldwind.ogc.wms;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+import javax.xml.namespace.QName;
+
+import gov.nasa.worldwind.util.xml.XmlElementModel;
+import gov.nasa.worldwind.util.xml.XmlPullParserContext;
+
+public class WmsAddress extends XmlElementModel
+{
+    protected QName ADDRESS_TYPE;
+    protected QName ADDRESS;
+    protected QName CITY;
+    protected QName STATE_OR_PROVINCE;
+    protected QName POST_CODE;
+    protected QName COUNTRY;
+
+    protected String addressType;
+    protected String address;
+    protected String city;
+    protected String stateOrProvince;
+    protected String postCode;
+    protected String country;
+
+    public WmsAddress(String namespaceURI)
+    {
+        super(namespaceURI);
+
+        this.initialize();
+    }
+
+    protected void initialize()
+    {
+        ADDRESS_TYPE = new QName(this.getNamespaceUri(), "AddressType");
+        ADDRESS = new QName(this.getNamespaceUri(), "Address");
+        CITY = new QName(this.getNamespaceUri(), "City");
+        STATE_OR_PROVINCE = new QName(this.getNamespaceUri(), "StateOrProvince");
+        POST_CODE = new QName(this.getNamespaceUri(), "PostCode");
+        COUNTRY = new QName(this.getNamespaceUri(), "Country");
+    }
+
+    @Override
+    protected void doParseEventContent(XmlPullParserContext ctx)
+        throws XmlPullParserException, IOException
+    {
+
+        XmlPullParser xpp = ctx.getParser();
+
+        if (xpp.getEventType() == XmlPullParser.START_TAG) {
+
+            QName event = new QName(xpp.getNamespace(), xpp.getName());
+
+            if (event.equals(ADDRESS_TYPE)) {
+                if (xpp.next() == XmlPullParser.TEXT) {
+                    this.setAddressType(xpp.getText().trim());
+                }
+            } else if (event.equals(ADDRESS)) {
+                if (xpp.next() == XmlPullParser.TEXT) {
+                    this.setAddress(xpp.getText().trim());
+                }
+            } else if (event.equals(CITY)) {
+                if (xpp.next() == XmlPullParser.TEXT) {
+                    this.setCity(xpp.getText().trim());
+                }
+            } else if (event.equals(STATE_OR_PROVINCE)) {
+                if (xpp.next() == XmlPullParser.TEXT) {
+                    this.setStateOrProvince(xpp.getText().trim());
+                }
+            } else if (event.equals(POST_CODE)) {
+                if (xpp.next() == XmlPullParser.TEXT) {
+                    this.setPostCode(xpp.getText().trim());
+                }
+            } else if (event.equals(COUNTRY)) {
+                if (xpp.next() == XmlPullParser.TEXT) {
+                    this.setCountry(xpp.getText().trim());
+                }
+            }
+
+        }
+    }
+
+    public String getAddressType()
+    {
+        return addressType;
+    }
+
+    protected void setAddressType(String addressType)
+    {
+        this.addressType = addressType;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    protected void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    protected void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public String getStateOrProvince()
+    {
+        return stateOrProvince;
+    }
+
+    protected void setStateOrProvince(String stateOrProvince)
+    {
+        this.stateOrProvince = stateOrProvince;
+    }
+
+    public String getPostCode()
+    {
+        return postCode;
+    }
+
+    protected void setPostCode(String postCode)
+    {
+        this.postCode = postCode;
+    }
+
+    public String getCountry()
+    {
+        return country;
+    }
+
+    protected void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    @Override
+    public XmlElementModel newInstance() {
+        return new WmsAddress(this.getNamespaceUri());
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("AddressType: ").append(this.addressType != null ? this.addressType : "none").append(" ");
+        sb.append("Address: ").append(this.address != null ? this.address : "none").append(" ");
+        sb.append("City: ").append(this.city != null ? this.city : "none").append(" ");
+        sb.append("StateOrProvince: ").append(this.stateOrProvince != null ? this.stateOrProvince : "none").append(" ");
+        sb.append("PostCode: ").append(this.postCode != null ? this.postCode : "none").append(" ");
+        sb.append("Country: ").append(this.country != null ? this.country : "none");
+
+        return sb.toString();
+    }
+}
