f32e59a9b0f906fa90ff6148bf3fd36b6555b006
pdavidc <paul@paulcollinssoftware.com>
2017-04-19 10:39:37 -0700

    Removed class WmsElevationCoverage after code review

--- worldwind/src/main/java/gov/nasa/worldwind/globe/BasicElevationCoverage.java
+++ worldwind/src/main/java/gov/nasa/worldwind/globe/BasicElevationCoverage.java
@@ -5,8 +5,11 @@
 
 package gov.nasa.worldwind.globe;
 
-import gov.nasa.worldwind.geom.Sector;
-import gov.nasa.worldwind.ogc.WmsElevationCoverage;
+import gov.nasa.worldwind.ogc.WmsLayerConfig;
+import gov.nasa.worldwind.ogc.WmsTileFactory;
+import gov.nasa.worldwind.util.LevelSet;
+import gov.nasa.worldwind.util.LevelSetConfig;
+import gov.nasa.worldwind.util.Logger;
 
 /**
  * Displays NASA's global elevation coverage at 10m resolution within the continental United States, 30m resolution in
@@ -14,7 +17,7 @@ import gov.nasa.worldwind.ogc.WmsElevationCoverage;
  * BasicElevationCoverage is configured to retrieve elevation coverage from the WMS at <a
  * href="https://worldwind26.arc.nasa.gov/wms?SERVICE=WMS&REQUEST=GetCapabilities">https://worldwind26.arc.nasa.gov/wms</a>.
  */
-public class BasicElevationCoverage extends WmsElevationCoverage {
+public class BasicElevationCoverage extends TiledElevationCoverage {
 
     /**
      * Constructs a global elevation coverage with the WMS at https://worldwind26.arc.nasa.gov/wms.
@@ -31,6 +34,19 @@ public class BasicElevationCoverage extends WmsElevationCoverage {
      * @throws IllegalArgumentException If the service address is null
      */
     public BasicElevationCoverage(String serviceAddress) {
-        super(new Sector().setFullSphere(), 13, serviceAddress, "GEBCO,aster_v2,USGS-NED");
+        if (serviceAddress == null) {
+            throw new IllegalArgumentException(
+                Logger.logMessage(Logger.ERROR, "BasicElevationCoverage", "constructor", "missingServiceAddress"));
+        }
+
+        LevelSetConfig levelSetConfig = new LevelSetConfig();
+        levelSetConfig.numLevels = 13;
+        this.setLevelSet(new LevelSet(levelSetConfig));
+
+        WmsLayerConfig layerConfig = new WmsLayerConfig();
+        layerConfig.serviceAddress = serviceAddress;
+        layerConfig.layerNames = "GEBCO,aster_v2,USGS-NED";
+        layerConfig.imageFormat = "application/bil16";
+        this.setTileFactory(new WmsTileFactory(layerConfig));
     }
 }
