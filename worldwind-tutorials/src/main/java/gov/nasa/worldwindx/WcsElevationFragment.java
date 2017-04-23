666b72ac9ca5f596020ec6e24080f9bac327c955
Zach Glueckert <zachrg@gmail.com>
2017-04-23 15:42:16 -0400

    Update coverage type and point of interest

--- worldwind-tutorials/src/main/java/gov/nasa/worldwindx/WcsElevationFragment.java
+++ worldwind-tutorials/src/main/java/gov/nasa/worldwindx/WcsElevationFragment.java
@@ -7,9 +7,10 @@ package gov.nasa.worldwindx;
 
 import gov.nasa.worldwind.WorldWind;
 import gov.nasa.worldwind.WorldWindow;
-import gov.nasa.worldwind.geom.LookAt;
+import gov.nasa.worldwind.geom.Camera;
 import gov.nasa.worldwind.geom.Position;
 import gov.nasa.worldwind.geom.Sector;
+import gov.nasa.worldwind.globe.ElevationCoverage;
 import gov.nasa.worldwind.globe.Globe;
 import gov.nasa.worldwind.ogc.Wcs100ElevationCoverage;
 
@@ -26,24 +27,24 @@ public class WcsElevationFragment extends BasicGlobeFragment {
         WorldWindow wwd = super.createWorldWindow();
 
         // Specify the bounding sector - provided by the WCS
-        Sector coverageSector = Sector.fromDegrees(-83.0, -180.0, 173.0, 360.0);
+        Sector coverageSector = Sector.fromDegrees(25.0, -125.0, 25.0, 60.0);
         // Specify the number of levels to match data resolution
         int numberOfLevels = 12;
         // Specify the version 1.0.0 WCS address
         String serviceAddress = "https://worldwind26.arc.nasa.gov/wcs";
         // Specify the coverage name
-        String coverage = "aster_v2";
+        String coverage = "USGS-NED";
 
         // Create an elevation coverage from a version 1.0.0 WCS
-        Wcs100ElevationCoverage aster = new Wcs100ElevationCoverage(coverageSector, numberOfLevels, serviceAddress, coverage);
+        ElevationCoverage usgsNed = new Wcs100ElevationCoverage(coverageSector, numberOfLevels, serviceAddress, coverage);
 
         // Remove any existing coverages from the Globe
         wwd.getGlobe().getElevationModel().clearCoverages();
 
         // Add the coverage to the Globes elevation model
-        wwd.getGlobe().getElevationModel().addCoverage(aster);
+        wwd.getGlobe().getElevationModel().addCoverage(usgsNed);
 
-        // Position the camera to look at the Sangre de Cristo Mountains
+        // Position the camera to look at Mt. Rainier
         this.positionView(wwd);
 
         return wwd;
@@ -51,23 +52,24 @@ public class WcsElevationFragment extends BasicGlobeFragment {
 
     protected void positionView(WorldWindow wwd) {
 
-        Position blancaPeak = new Position(37.577227, -105.485845, 4374);
-        Position eye = new Position(37.5, -105.4, 5000);
+        Position mtRainier = new Position(46.852886, -121.760374, 4392.0);
+        Position eye = new Position(46.912, -121.527, 2000.0);
 
         // Compute heading and distance from peak to eye
         Globe globe = wwd.getGlobe();
-        double heading = eye.greatCircleAzimuth(blancaPeak);
-        double distanceRadians = blancaPeak.greatCircleDistance(eye);
-        double distance = distanceRadians * globe.getRadiusAt(blancaPeak.latitude, blancaPeak.longitude);
+        double heading = eye.greatCircleAzimuth(mtRainier);
+        double distanceRadians = mtRainier.greatCircleDistance(eye);
+        double distance = distanceRadians * globe.getRadiusAt(mtRainier.latitude, mtRainier.longitude);
 
         // Compute camera settings
-        double altitude = eye.altitude - blancaPeak.altitude;
+        double altitude = eye.altitude - mtRainier.altitude;
         double range = Math.sqrt(altitude * altitude + distance * distance);
         double tilt = Math.toDegrees(Math.atan(distance / eye.altitude));
 
         // Apply the new view
-        LookAt lookAt = new LookAt();
-        lookAt.set(blancaPeak.latitude, blancaPeak.longitude, blancaPeak.altitude, WorldWind.ABSOLUTE, range, heading, tilt, 0 /*roll*/);
-        wwd.getNavigator().setAsLookAt(globe, lookAt);
+        Camera camera = new Camera();
+        camera.set(eye.latitude, eye.longitude, eye.altitude, WorldWind.ABSOLUTE, heading, tilt, 0.0 /*roll*/);
+
+        wwd.getNavigator().setAsCamera(globe, camera);
     }
 }
