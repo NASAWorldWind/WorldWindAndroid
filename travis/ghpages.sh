01727edfd7c8656f3b19da591083696f675edd3e
Zach Glueckert <zachrg@gmail.com>
2017-05-23 21:12:40 -0400

    Refactor travis procedures - Remove daily build publishing - Utilize travis deploy capabilities - Simplify javadoc publishing

--- travis/ghpages.sh
+++ travis/ghpages.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 # ======================================================================================================================
 # Updates the GitHub Pages website with the updated javadoc from the build. Clones the GitHub Pages to the local
-# filesystem, deletes the javadoc for the current build, copies the new javadoc, then commits and pushes the changes.
+# filesystem, copies the new javadoc, then commits and pushes the changes.
 #
 # Uses Git to update tags in the repo. Git commands using authentication are redirected to /dev/null to prevent leaking
 # the access token into the log.
 # ======================================================================================================================
 
-# Ensure shell commands are not echoed in the log to prevent leaking the access token.
+#!/bin/bash
+
 set +x
+set -e
+
+echo '[ghpages] TRAVIS_TAG='$TRAVIS_TAG
+echo '[ghpages] TRAVIS_BRANCH='$TRAVIS_BRANCH
+echo '[ghpages] TRAVIS_PULL_REQUEST='$TRAVIS_PULL_REQUEST
+
+echo "Updating the GitHub pages repository and site with latest build documentation..."
 
 # Assert the GitHub Personal Access Token exists
 if [[ -z "$GITHUB_API_KEY" ]]; then
@@ -22,19 +30,8 @@ if [[ -z "$GH_PAGES_REPO" ]]; then
     exit 1
 fi
 
-# Initialize the FOLDER var predicated on the build configuration. On a tagged build, Travis cloned the repo into a
-# branch named with the tag, thus the TRAVIS_BRANCH var reflects the tag name, not "master" or "develop" like you might
-# expect.
-if [[ "${TRAVIS_TAG}" == "daily"* ]]; then # daily build associated with a tag in the format daily/YYYYMMDD
-    FOLDER="daily"
-elif [[ "${TRAVIS_BRANCH}" == "master" ]]; then # latest stable build from the master branch
-    FOLDER="latest"
-else # all other build types; exit quietly without error
-    exit 0
-fi
-
 # Emit a log message for the javadoc update
-echo "Updating JavaDoc at ${GH_PAGES_REPO}/assets/android/${FOLDER}/javadoc"
+echo "Updating JavaDoc at ${GH_PAGES_REPO}/assets/android/${TRAVIS_TAG}/javadoc"
 
 # Configure the user to be associated with commits to the GitHub pages
 git config --global user.email "travis@travis-ci.org"
@@ -42,15 +39,18 @@ git config --global user.name "travis-ci"
 
 # Clone the GitHub Pages repository to the local filesystem
 GH_PAGES_DIR=${HOME}/gh_pages
-git clone --quiet --branch=master https://${GITHUB_API_KEY}@${GH_PAGES_REPO} $GH_PAGES_DIR > /dev/null
+git clone --quiet --branch=master https://${GITHUB_API_KEY}@github.com/f17d911/NASAWorldWind.github.io.git $GH_PAGES_DIR > /dev/null
 cd $GH_PAGES_DIR
 
-# Remove existing javadocs from the repository
-git rm -rfq --ignore-unmatch ./assets/android/${FOLDER}/javadoc
-
 # Copy new javadocs to the repository
-mkdir -p ./assets/android/${FOLDER}/javadoc
-cp -Rf ${TRAVIS_BUILD_DIR}/worldwind/build/outputs/doc/javadoc/* ./assets/android/${FOLDER}/javadoc
+mkdir -p ./assets/android/${TRAVIS_TAG}/javadoc
+cp -Rf ${TRAVIS_BUILD_DIR}/worldwind/build/outputs/doc/javadoc/* ./assets/android/${TRAVIS_TAG}/javadoc
+
+# Initialize the FOLDER var predicated on the build configuration
+RELEASES_URL="https://api.github.com/repos/f17d911/worldwindandroid/releases"
+
+# Update the release log to reflect all versions available
+curl --silent -o ./assets/android/releases.json --header "Authorization: token ${GITHUB_API_KEY}" ${RELEASES_URL}
 
 # Commit and push the changes (quietly)
 git add -f .
