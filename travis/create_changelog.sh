a34d773f5f2f70a0add72dbf59f49ae6b8c3cce4
Zach Glueckert <zachrg@gmail.com>
2017-05-24 12:22:40 -0400

    Add changelog generation on tagged build -See #104

---
+++ travis/create_changelog.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# ======================================================================================================================
+# Creates a changelog using the GitHub API and the milestone associated with the tag.
+# ======================================================================================================================
+
+set +x
+
+# Install jq to filter GitHub API results
+sudo apt-get install -qq jq
+jq --version
+
+# GitHub RESTful API URLs
+GITHUB_API_URL="https://api.github.com/repos/nasaworldwind/worldwindandroid"
+
+# Query GitHub for a milestone matching the Git tag
+milestone_array=( \
+    $(curl --silent "${GITHUB_API_URL}/milestones?state=all" \
+    | jq --arg title "${TRAVIS_TAG}" '.[] | select(.title == $title) | .number') \
+    ) > /dev/null
+
+# When GitHub has milestones associated with the release, assemble a Changelog file and post it to the release assets
+if [[ "${#milestone_array[@]}" -ne 0 ]]; then
+    # Assemble Markdown headers for the Changelog file
+    echo "#Changelog" >> CHANGELOG.md
+    echo "##${TRAVIS_TAG}" >> CHANGELOG.md
+
+    # Assemble Markdown list items for each milestone's associated issues
+    for milestone in "${milestone_array[*]}"
+    do
+        issueArray=$(curl --silent "${GITHUB_API_URL}/issues?state=all&milestone=${milestone}" | jq -c '.[] | [.title, .number, .html_url]') >> /dev/null
+        while read line
+        do
+            echo ${line} | sed 's#\["\(.*\)",\(.*\),"\(.*\)"\]#- \1 ([\#\2](\3))#' >> CHANGELOG.md
+        done <<< "${issueArray[*]}"
+    done
+fi
