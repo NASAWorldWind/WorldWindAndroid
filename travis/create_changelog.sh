273a658e9e7af2134bdf9a044b7cba7c5eae57d1
Zach Glueckert <zachrg@gmail.com>
2017-05-31 16:57:46 -0400

    Add meta data to CHANGELOG notes

--- travis/create_changelog.sh
+++ travis/create_changelog.sh
@@ -5,6 +5,12 @@
 
 set +x
 
+# Assemble Markdown headers for the Changelog file
+echo "#Changelog" >> CHANGELOG.md
+echo "##${TRAVIS_TAG}" >> CHANGELOG.md
+FORMATTED_DATE=$(date '+%Y-%b-%d')
+echo "###${FORMATTED_DATE}" >> CHANGELOG.md
+
 # Install jq to filter GitHub API results
 sudo apt-get install -qq jq
 jq --version
@@ -18,19 +24,27 @@ MILESTONE_ARRAY=( \
     | jq --arg title "${TRAVIS_TAG}" '.[] | select(.title == $title) | .number') \
     ) > /dev/null
 
+# Write the milestone description into the Changelog
+MILESTONE_DESCRIPTION=( \
+    $(curl --silent "${GITHUB_API_URL}/milestones/${MILESTONE_ARRAY[0]}" \
+    | jq .description) \
+    ) > /dev/null
+MILESTONE_DESCRIPTION=${MILESTONE_DESCRIPTION[*]#\"}
+echo ${MILESTONE_DESCRIPTION[*]%\"} >> CHANGELOG.md
+
+# Write instruction for how to get the library from JCenter
+echo "###Use via JCenter and Gradle" >> CHANGELOG.md
+echo "\`\`\`groovy" >> CHANGELOG.md
+echo "compile: \"gov.nasa.worldwind.andriod:worldwind:${TRAVIS_TAG}@aar\"" >> CHANGELOG.md
+echo "\`\`\`" >> CHANGELOG.md
+echo "The JCenter repository must be specified in your project (Android Studio does this as a default)." >> CHANGELOG.md
+
 # When GitHub has milestones associated with the release, assemble a Changelog file and post it to the release assets
 if [[ "${#MILESTONE_ARRAY[@]}" -ne 0 ]]; then
-    # Assemble Markdown headers for the Changelog file
-    echo "#Changelog" >> CHANGELOG.md
-    echo "##${TRAVIS_TAG}" >> CHANGELOG.md
-
     # Assemble Markdown list items for each milestone's associated issues
-    for MILESTONE in "${MILESTONE_ARRAY[*]}"
+    ISSUE_ARRAY=$(curl --silent "${GITHUB_API_URL}/issues?state=all&milestone=${MILESTONE_ARRAY[0]}" | jq -c '.[] | [.title, .number, .html_url]') >> /dev/null
+    while read line
     do
-        ISSUE_ARRAY=$(curl --silent "${GITHUB_API_URL}/issues?state=all&milestone=${MILESTONE}" | jq -c '.[] | [.title, .number, .html_url]') >> /dev/null
-        while read line
-        do
-            echo ${line} | sed 's#\["\(.*\)",\(.*\),"\(.*\)"\]#- \1 ([\#\2](\3))#' >> CHANGELOG.md
-        done <<< "${ISSUE_ARRAY[*]}"
-    done
+        echo ${line} | sed 's#\["\(.*\)",\(.*\),"\(.*\)"\]#- \1 ([\#\2](\3))#' >> CHANGELOG.md
+    done <<< "${ISSUE_ARRAY[*]}"
 fi
